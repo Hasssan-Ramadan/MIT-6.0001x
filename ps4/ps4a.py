7742b2e47063ed1cfa70890cd5f20943eda55ce1
Hassan Ramadan <HassanRamadanEbrahim@gmail.com>
2021-03-05 17:28:25 +0200

    Add Part A: Permutations of a string solution

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -2,6 +2,8 @@
 # Name: <your name here>
 # Collaborators:
 # Time Spent: x:xx
+import unittest
+
 
 def get_permutations(sequence):
     '''
@@ -22,19 +24,31 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    if len(sequence) == 1:
+        return [sequence]
+    permutations = []
+    for index, char in enumerate(sequence):
+        for permutation in get_permutations(sequence[:index] + sequence[index + 1:]):
+            permutations += [char + permutation]
+    return permutations
 
-    pass #delete this line and replace with your code here
+
+class TestPermutations(unittest.TestCase):
+
+    def test_permutations(self, squence, expected):
+        self.assertEqual(get_permutations(squence), expected,
+                         "Actual and expected permutations are not the same")
+
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
+    #    #EXAMPLE
+    #    example_input = 'abc'
+    #    print('Input:', example_input)
+    #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    #    print('Actual Output:', get_permutations(example_input))
 
-    pass #delete this line and replace with your code here
-
+    #    # Put three example test cases here (for your sanity, limit your inputs
+    #    to be three characters or fewer as you will have n! permutations for a
+    #    sequence of length n)
+    test = TestPermutations()
+    test.test_permutations('nor', ['nor', 'nro', 'onr', 'orn', 'rno', 'ron'])
